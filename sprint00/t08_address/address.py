279ec218d7240241165e1b587ffac76c20489588
kamilla-boiko <kamilla.boyko@gmail.com>
2022-04-06 17:57:46 +0300

    task08 done

---
+++ sprint00/t08_address/address.py
@@ -0,0 +1,8 @@
+var1 = 1000
+var2 = 1000
+var3 = 999
+print(f"var1 = {var1}, address is {id(var1)}")
+print(f"var2 = {var2}, address is {id(var2)}")
+print(f"var3 = {var3}, address is {id(var3)}")
+print(f"var1 is var2 = {var1 is var2}")
+print(f"var2 is var3 = {var2 is var3}")
