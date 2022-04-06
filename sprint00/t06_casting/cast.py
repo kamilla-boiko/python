67d7f2920b9488da593081a9fa9c0da29093fdaf
kamilla-boiko <kamilla.boyko@gmail.com>
2022-04-06 16:11:58 +0300

    task06: change output view
    The task was understood incorrectly, the print was without preliminary explanations.
    This change fixed my mistake.

--- sprint00/t06_casting/cast.py
+++ sprint00/t06_casting/cast.py
@@ -2,7 +2,14 @@ a = 3
 b = 10
 c = -14.8
 d = True
-print(float(a) + float(b))
-print(int(c) - int(b))
-print(str(c) + str(b))
-print(bool(a - a))
+print(f"""Available variables:
+a = {a}
+b = {b}
+c = {c}
+d = {d}
+""")
+print("Result:")
+print(f"{a} + {b} =", float(a) + float(b))
+print(f"{c} - {b} =", int(c) - int(b))
+print(f"{c} + {b} =", str(c) + str(b))
+print(f"{a} - {a} =", bool(a - a))
