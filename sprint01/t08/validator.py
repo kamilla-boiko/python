537c120f2f71297efe32fe4c297932d170a9491a
kamilla-boiko <kamilla.boyko@gmail.com>
2022-04-10 21:14:56 +0300

    task08 done

--- sprint01/t08/validator.py
+++ sprint01/t08/validator.py
@@ -1,1 +1,36 @@
-print(123)
+def validator(arg):
+	expression = str.split(arg, ' ')
+
+	if len(expression) < 3:
+		return False
+
+	try:
+		num1 = float(expression[0])
+		num2 = float(expression[2])
+		op = expression[1]
+		if op != '>' and op != '<' and op != '>=' and op != '<=' and op != '==':
+		   return False
+	except ValueError:
+		return False
+
+	if (op == '>') and (num1 < num2):
+		return f"{num1} < {num2}"
+	if op == '<' and (num1 > num2):
+		return f"{num1} > {num2}"
+	if op == '==' and (num1 != num2):
+		if num1 <= num2:
+			return f"{num1} <= {num2}"
+		else:
+			return f"{num1} >= {num2}"
+	if op == '<=' and (num1 >= num2):
+		if num1 == num2:
+			return f"{num1} == {num2}"
+		else:
+			return f"{num1} >= {num2}"
+	if op == '>=' and (num1 <= num2):
+		if num1 == num2:
+			return f"{num1} == {num2}"
+		else:
+			return f"{num1} <= {num2}"
+	else:
+		return True
